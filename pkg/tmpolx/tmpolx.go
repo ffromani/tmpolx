ab0694c55d6350a25e879c76760d6f7d9d8ac180
Francesco Romani <fromani@redhat.com>
2022-09-06 19:15:03 +0200

    tmpolx: docs: add notes about TM policies
    We can use so more notes here, so let's gather knowledge scattered
    in k/k issues, public slack chats and so forth
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- pkg/tmpolx/tmpolx.go
+++ pkg/tmpolx/tmpolx.go
@@ -65,6 +65,33 @@ func (tmpx *TMPolx) String() string {
 	return fmt.Sprintf("using policy %q\n%s", tmpx.policy.Name(), buf.String())
 }
 
+/*
+> From: https://github.com/kubernetes/kubernetes/issues/84597#issuecomment-548414942
+
+The restricted policy operates by limiting preferred alignments to the minimum possible alignment for the given request size on the given machine.
+
+For your machine, this means that:
+
+    Request sizes <= 6 will be restricted to a single NUMA node.
+    Request sizes 7-12 will be restricted to 2 NUMA nodes.
+    Request sizes 12-18 will be restricted to 3 NUMA nodes.
+
+For your exact example, since there exists a way to allocate 3 CPUs from a single NUMA node on your machine (e.g. when no other pods are running), then requests of size 3 are restricted to single NUMA alignment for all pods.
+
+This differs from the single-numa-node policy in that, no matter what the machine configuration looks like you must have alignment on a single NUMA node in order for the pod to be admitted. In your setup, this would mean that requests of sizes 7-18 would never have a path to admission.
+
+The semantics you seem to be expecting are part of the best-effort policy, which will attempt to align on as few NUMA nodes as possible, only spilling over to another one if necessary.
+
+---
+
+> From: https://kubernetes.slack.com/archives/C0BP8PW9G/p1661761032814389?thread_ts=1661680145.406899&cid=C0BP8PW9G
+
+The three policies are:
+single-numa-node: only allow allocations from a single NUMA node, fail otherwise. Even if one of the requested resource requires more than one NUMA node to be satisfied.
+restricted: only allow allocations from the minimum number of NUMA nodes. Look at each resource request, see what the minimum number of NUMA nodes are required to satisfy that resource request. Allow alignment to that number of NUMA nodes for all resources. Fail otherwise.
+best-effort: Run as restricted, but never fail the allocation. Fall back to allocating from any remaining NUMA nodes as necessary. (edited)
+*/
+
 func NewFromParams(params Params) (*TMPolx, error) {
 	if len(params.NUMANodes) > MaxNUMANodes {
 		return nil, fmt.Errorf("TM currently supports up to %d NUMA nodes (got %d)", MaxNUMANodes, len(params.NUMANodes))
